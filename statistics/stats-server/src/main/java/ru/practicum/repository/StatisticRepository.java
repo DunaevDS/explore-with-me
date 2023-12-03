aafb0ef3eac081334dc1f674331ade236f80288a
DunaevDS <125894370+DunaevDS@users.noreply.github.com>
2023-12-03 15:20:52 +0500

    1st Review

--- statistics/stats-server/src/main/java/ru/practicum/repository/StatisticRepository.java
+++ statistics/stats-server/src/main/java/ru/practicum/repository/StatisticRepository.java
@@ -9,30 +9,8 @@ import ru.practicum.model.Statistic;
 import java.time.LocalDateTime;
 import java.util.List;
 
-
-//Сделал как Вы и говорили, но при таком коде валится 1 из тестов и я не понимаю причину
-//Тест "В теле ответа должна присутствовать сортировка по убыванию количества просмотров"
 @Repository
 public interface StatisticRepository extends JpaRepository<Statistic, Long> {
-/*    @Query("SELECT new ru.practicum.dto.StatisticViewDto(s.app, s.uri, COUNT(DISTINCT s.ip)) " +
-            "FROM Statistic AS s " +
-            "WHERE s.timestamp BETWEEN :start AND :end " +
-            "AND (s.uri IN :uris OR :uris = NULL) " +
-            "GROUP BY s.app, s.uri " +
-            "ORDER BY COUNT(DISTINCT s.ip) DESC ")
-    List<StatisticViewDto> findUniqueStatistic(LocalDateTime start,
-                                    LocalDateTime end,
-                                    List<String> uris);
-
-    @Query("SELECT new ru.practicum.dto.StatisticViewDto(s.app, s.uri, COUNT(s.ip)) " +
-            "FROM Statistic AS s " +
-            "WHERE s.timestamp BETWEEN :start AND :end " +
-            "AND (s.uri IN :uris OR :uris = NULL) " +
-            "GROUP BY s.app, s.uri " +
-            "ORDER BY COUNT(DISTINCT s.ip) DESC ")
-    List<StatisticViewDto> findStatistic(LocalDateTime start,
-                                               LocalDateTime end,
-                                               List<String> uris);*/
 
     @Query("SELECT new ru.practicum.dto.StatisticViewDto(s.app, s.uri, COUNT (s.ip))" +
             "FROM Statistic AS s " +
